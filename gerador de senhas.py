c04d1de7a8c483a57543214e637f764416825c81
Natanael Silva <nael0lens@gmail.com>
2022-06-30 17:54:02 -0300

    commit versão 2

--- gerador de senhas.py
+++ gerador de senhas.py
@@ -4,6 +4,6 @@ minusculas ="abcdefghijklmnopqrstuvwxyz"
 numeros ="0123456789"
 caracteres_especiais ="{}[]()*:;?.,_-"
 tudo = maiusculas+minusculas+numeros+caracteres_especiais
-tamanho = 8
-senha = "".join(random.sample(tudo,tamanho))
+tamanho = int(input("Diga qual tamanho a senha deve ter: "))
+senha = " ".join(random.sample(tudo,tamanho))
 print(senha)
